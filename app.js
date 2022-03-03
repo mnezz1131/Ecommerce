975c4e4050a1072d13bcecc715b2303035c7dcb2
Mike <mnezz1131@gmail.com>
2022-03-03 08:15:47 -0600

    mondoDB connected

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+require("dotenv").config();
+
+//import routes
+const userRoutes = require('./routes/user.js')
+
+//app
+const app = express();
+
+//db - Use mongoose to connect MongoDB
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  // useCreateIndex: true
+}).then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//routes middleware - prepends /api to localhost:8000
+app.use('/api', userRoutes)
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+  console.log(`The Audience is listening on PORT ${port}`)
+})
+
+
+
